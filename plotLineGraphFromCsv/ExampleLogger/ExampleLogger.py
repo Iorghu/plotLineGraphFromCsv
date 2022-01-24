d9f8b1b88c4e21f334fbf5f75818bedb39a289bf
Georg <Iorghu@users.noreply.github.com>
2022-01-24 18:48:02 +0100

    Update ExampleLogger.py

--- plotLineGraphFromCsv/ExampleLogger/ExampleLogger.py
+++ plotLineGraphFromCsv/ExampleLogger/ExampleLogger.py
@@ -2,8 +2,9 @@ import psutil
 import datetime
 import os
 
-fileName = "myLoggedData.csv"
+fileName = "myLoggedData.csv" # Create this file (empty) before running the app. 
 
+# adding the header.
 if os.stat(fileName).st_size==0:
     file1=open(fileName,'a')
     file1.write("date,cpu_frequency,cpu_percentage,cpu1,memory_percent\n")
@@ -14,12 +15,12 @@ while True:
     currentState=[]
     currentState.append(str(datetime.datetime.now()))
     currentState.append(psutil.cpu_freq().current / 100) # scaling down, to fit in rest of the values.
-    cpu_percent=psutil.cpu_percent(interval=15, percpu=True)
+    cpu_percent=psutil.cpu_percent(interval=15, percpu=True) # since interval=15 is used, there is no need to sleep.
     sum=0
     for i in cpu_percent:
         sum+=i
     currentState.append(sum/len(cpu_percent))
-    currentState.append(cpu_percent[0])
+    currentState.append(cpu_percent[0]) # just to compare a little bit.
     currentState.append(psutil.virtual_memory().percent)
     x=",".join(map(str,currentState)) + "\n"
     print("currentState = ")
