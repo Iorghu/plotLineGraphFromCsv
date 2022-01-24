28d1d5793c54c095a3ca74af47152ab7c2abdb74
Georg <Iorghu@users.noreply.github.com>
2022-01-24 18:44:11 +0100

    Update plotLineGraphFromCsv.py

--- plotLineGraphFromCsv/plotLineGraphFromCsv.py
+++ plotLineGraphFromCsv/plotLineGraphFromCsv.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import time
 
-# TODO: commentaries
+# filename of the .csv file to be showed, can be changed via arguments. Be sure that the external app writes its data into the same file.
 if len(sys.argv) > 1:
     print("argument found, using '" + str(sys.argv[1]) + "' as name of the csv.")
     nameOfCsv = sys.argv[1]
@@ -13,11 +13,11 @@ else:
     nameOfCsv = "myLoggedData.csv"
     print("No argument found, using default filename: " + nameOfCsv)
     
-webbrowser.open("autoUpdate.html") # opens a local html file, that reloads the graph every 15 seconds.
+webbrowser.open("autoUpdate.html") # opens a local html file, that reloads the graph every x seconds.
 
 while True:
     df = pd.read_csv(nameOfCsv)
     cols = df.columns
     fig = px.line(df,x=cols[0],y=cols[1:])
-    fig.write_html("graph.html")
+    fig.write_html("graph.html") # this file will be reloaded in autoUpdate.html
     time.sleep(10)
